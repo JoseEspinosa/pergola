589de90fce6448355de1b00e49691938445cbf68
JoseEspinosa <kadomu@gmail.com>
2017-12-20 11:50:23 +0100

    new update

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from sys import exit
 
 PATH = path.abspath(path.split(path.realpath(__file__))[0])
 
-VERSION = "0.17.0"
+VERSION = "0.18.0"
 
 TAGS = [
     "Development Status :: 2 - Pre-Alpha",
